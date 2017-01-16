596edd475efcc89383115bee2a677f47f3ee23b3
GobbletDev <8bd45610@10minutemail.co.uk>
2017-01-16 18:21:50 +0200

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,17 @@
+$(window).load(function() {
+  var config = {
+    width: 3,
+    height: 3
+  }
+
+
+  for (var i = 0; i < config.height; i++) {
+    var $row = $("<div>").addClass("row");
+    for (var j = 0; j < config.width; j++) {
+      var $box = $("<div>").addClass("box");
+      $row.append($box);
+    }
+    $(".board").append($row);
+
+}
+});
