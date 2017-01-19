319ef094f1314c5ee3de4a163796197bd077c14b
GobbletDev <8bd45610@10minutemail.co.uk>
2017-01-19 18:19:01 +0200

    Add files via upload

--- Game.js
+++ Game.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 //board size config
 $(window).load(function() {
   var config = {
@@ -26,29 +26,8 @@ for (var i = 0; i < config.height; i++) {
 for (var i = 0; i < config.height; i++) {
   for (var j = 0; j < config.width; j++) {
     GameBoard[i][j]=0;
-
   }
 }
+
 console.log(GameBoard);
 });
-=======
-$(window).load(function() {
-  var config = {
-    width: 3,
-    height: 3
-  }
-
-
-  for (var i = 0; i < config.height; i++) {
-    var $row = $("<div>").addClass("row");
-    for (var j = 0; j < config.width; j++) {
-      var $box = $("<div>").addClass("box");
-      $row.append($box);
-    }
-    $(".board").append($row);
-    var $gobblerBig = $("<div>").addClass("gobblerBig");
-    $(".board").append($gobblerBig);
-
-}
-});
->>>>>>> origin/master
