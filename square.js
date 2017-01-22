15479c7f1d46a94fe944014fb6482ddbae44745c
GobbletDev <8bd45610@10minutemail.co.uk>
2017-01-22 21:38:44 +0200

    CurrentGom

--- square.js
+++ square.js
@@ -18,7 +18,7 @@ class square {
       SmlGob = 1- blue 2- red
       MedGob = 1- blue 2- red
       BigGob = 1- blue 2- red
-      Value = 1- blue 10- red */ 
+      Value = 1- blue 10- red */
   set Move(x){
     if (x>0){
     switch (x) {
@@ -28,44 +28,44 @@ class square {
         return "Small Blue Gobbler"
       }
         else
-        return "Small Blue Can't Move"
+        return "Small Blue Can't Move";
         break;
       case 4:  if (isEmpty()==true) {
           SmlGob = 2;
           Value = 10;
-          return "Small Red Gobbler"
+          return "Small Red Gobbler";
         }
           else
-          return "Small Red Can't Move"
+          return "Small Red Can't Move";
           break;
       case 2: if ((isEmpty() == true) || (MedGob ==0 && BigGob == 0)){
         MedGob = 1;
         Value=1;
-        return "Medium Blue Goblber"
+        return "Medium Blue Goblber";
       }
         else {
-          return "Medium Blue Can't Move"
+          return "Medium Blue Can't Move";
         } break;
     case 5: if ((isEmpty() == true) || (MedGob ==0 && BigGob == 0)){
       MedGob = 2;
       Value=10;
-      return "Medium Red Goblber"
+      return "Medium Red Goblber";
     }
       else
-        return "Medium Red Can't Move"
+        return "Medium Red Can't Move";
        break;
     case 3: if ((isEmpty() == true) || (MedGob ==0)){
       BigGob = 1 ;
       Value=1;
-      return "Big blue Goblber"
+      return "Big blue Goblber";
     }
       else
-        return "Big blue Can't Move"
+        return "Big blue Can't Move";
       break;
     case 6: if ((isEmpty() == true) || (MedGob ==0)){
       BigGob = 2 ;
       Value=10;
-      return "Big Red Goblber"
+      return "Big Red Goblber";
     }
       else
         return "Big Red Can't Move"
@@ -82,38 +82,57 @@ class square {
         break;
       case -2: MedGob=0;
         if (!isEmpty()){
-        return  Move(SmlGob)
+        return  Move(SmlGob);
         }
         else {
           Value = 0
-          return "Empty now"
+          return "Empty now";
         } break;
       case -5: MedGob=0;
         if (!isEmpty()){
-        return  Move(SmlGob)
+        return  Move(SmlGob);
         }
         else {
           Value = 0
-          return "Empty now"
+          return "Empty now";
         } break;
       case -3:BigGob=0;
        if (MedGob>0)
-        return  Move (MedGob)
+        return  Move (MedGob);
          else if (SmlGob>0)
-          return Move (SmlGob)
+          return Move (SmlGob);
        else
         Value = 0;
         break;
       case -6:BigGob=0;
        if (MedGob>0)
-        return  Move (MedGob)
+        return  Move (MedGob);
          else if (SmlGob>0)
-          return Move (SmlGob)
+          return Move (SmlGob);
        else
         Value = 0;
       break;
     }//End negative switch
   }//end of else
     }//End set move
-  }
+get Value (){
+  return this.Value;
 }
+get CurrentGob(){
+  if (isEmpty()==true)
+    return 0;
+  switch (BigGob) {
+    case 1: return 3; break;
+    case 2: return 6; break;
+  }
+  switch (MedGob) {
+    case 1: return 2; break;
+    case 2: return 5; break;
+  }
+  switch (SmlGob) {
+    case 1: return 1; break;
+    case 2: return 3; break;
+  }
+}//End of CurrentGob
+
+}//Enf of square
