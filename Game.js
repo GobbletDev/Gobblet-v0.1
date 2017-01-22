f8b30b379f470ff1293de29412ccf0bbc8dd921c
GobbletDev <8bd45610@10minutemail.co.uk>
2017-01-22 21:19:05 +0200

    square

--- Game.js
+++ Game.js
@@ -1,4 +1,7 @@
 
+var turn = 0;
+var place = 0;
+
 //board size config
 $(window).load(function() {
   var config = {
@@ -17,17 +20,26 @@ $(window).load(function() {
 }
 
 var $gobblerBig = $("<div>").addClass("gobblerBig");
-$(".board").append($gobblerBig);
+
+var $gobblerBig2 = $("<div>").addClass("gobblerBig2");
+$(".board").append($gobblerBig,$gobblerBig2);
+//Creating 2d array and zero it
 var GameBoard = [];
 for (var i = 0; i < config.height; i++) {
   GameBoard[i]= [];
 }
-
 for (var i = 0; i < config.height; i++) {
   for (var j = 0; j < config.width; j++) {
     GameBoard[i][j]=0;
   }
 }
-
 console.log(GameBoard);
+$(".box").click(function (){
+  place = $(".box").index(this);
+  var x=Math.floor(place/config.width);
+  var y = place % config.width;
+   console.log($(".box").index(this));
+   console.log(x+','+y);
+ });
+
 });
---
+++ square.js
@@ -0,0 +1,119 @@
+class square {
+  constructor() {
+    this.BigGob = 0;
+    this.MedGob = 0;
+    this.SmlGob = 0;
+    this.Value = 0;
+  }
+  get isEmpty(){
+     if (BigGob==0 && MedGob==0 && SmlGob==0){
+       return true;
+     }
+     else {
+       return false
+     }
+   }
+   /* 1-Small Blue. 2-Medium Blue 3-Big Blue.
+      4-Small Red. 5-Medium Red 6-Big Red
+      SmlGob = 1- blue 2- red
+      MedGob = 1- blue 2- red
+      BigGob = 1- blue 2- red
+      Value = 1- blue 10- red */ 
+  set Move(x){
+    if (x>0){
+    switch (x) {
+      case 1: if (isEmpty()==true) {
+        SmlGob = 1;
+        Value=1;
+        return "Small Blue Gobbler"
+      }
+        else
+        return "Small Blue Can't Move"
+        break;
+      case 4:  if (isEmpty()==true) {
+          SmlGob = 2;
+          Value = 10;
+          return "Small Red Gobbler"
+        }
+          else
+          return "Small Red Can't Move"
+          break;
+      case 2: if ((isEmpty() == true) || (MedGob ==0 && BigGob == 0)){
+        MedGob = 1;
+        Value=1;
+        return "Medium Blue Goblber"
+      }
+        else {
+          return "Medium Blue Can't Move"
+        } break;
+    case 5: if ((isEmpty() == true) || (MedGob ==0 && BigGob == 0)){
+      MedGob = 2;
+      Value=10;
+      return "Medium Red Goblber"
+    }
+      else
+        return "Medium Red Can't Move"
+       break;
+    case 3: if ((isEmpty() == true) || (MedGob ==0)){
+      BigGob = 1 ;
+      Value=1;
+      return "Big blue Goblber"
+    }
+      else
+        return "Big blue Can't Move"
+      break;
+    case 6: if ((isEmpty() == true) || (MedGob ==0)){
+      BigGob = 2 ;
+      Value=10;
+      return "Big Red Goblber"
+    }
+      else
+        return "Big Red Can't Move"
+      break;
+  }//End switch
+}//enf if positive
+  else {
+    switch (x) {
+      case -1: SmlGob = 0;
+        Value =0;
+        break;
+      case -4:SmlGob=0;
+        Value=0;
+        break;
+      case -2: MedGob=0;
+        if (!isEmpty()){
+        return  Move(SmlGob)
+        }
+        else {
+          Value = 0
+          return "Empty now"
+        } break;
+      case -5: MedGob=0;
+        if (!isEmpty()){
+        return  Move(SmlGob)
+        }
+        else {
+          Value = 0
+          return "Empty now"
+        } break;
+      case -3:BigGob=0;
+       if (MedGob>0)
+        return  Move (MedGob)
+         else if (SmlGob>0)
+          return Move (SmlGob)
+       else
+        Value = 0;
+        break;
+      case -6:BigGob=0;
+       if (MedGob>0)
+        return  Move (MedGob)
+         else if (SmlGob>0)
+          return Move (SmlGob)
+       else
+        Value = 0;
+      break;
+    }//End negative switch
+  }//end of else
+    }//End set move
+  }
+}
